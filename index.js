5716fa1c12f94bd79c427afef80f2cc71037f63a
AnnaPravdiukova <annagekata82@gmail.com>
2023-04-13 20:03:30 +0300

    added price and post order endpoint

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-const express = require('express')
-const { PrismaClient } = require('@prisma/client')
-const _ = require('lodash')
-const { hashPassword, verifyPassword } = require('./utils/hash')
-const app = express()
-const prisma = new PrismaClient()
-const jwt = require('jsonwebtoken')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const _ = require("lodash");
+const { hashPassword, verifyPassword } = require("./utils/hash");
+const app = express();
+const prisma = new PrismaClient();
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const isAuthorized = (req, res, next) => {
   try {
     const { authorization } = req.headers;
 
     if (!authorization) {
-      return res.status(403).json({ message: 'Unauthorized' })
+      return res.status(403).json({ message: "Unauthorized" });
     }
 
-    const [, token] = authorization.split(' ');
-
-    if (!jwt.verify(token, process.env.JWT_KEY)) {
-      return res.status(403)
+    const [, token] = authorization.split(" ");
+    const user = jwt.verify(token, process.env.JWT_KEY);
+    if (!user) {
+      return res.status(403);
     }
-    next()
+    req.user = user;
+    next();
   } catch (err) {
-    console.log('[error][isAuthorized]', err)
-    return res.status(403).json({ message: err.message })
+    console.log("[error][isAuthorized]", err);
+    return res.status(403).json({ message: err.message });
   }
-}
+};
 
-app.post('/users/create', async (req, res) => {
+app.post("/users/create", async (req, res) => {
   try {
-    const { email, username, password } = req.body
-    const hashedPassword = hashPassword(password)
+    const { email, username, password } = req.body;
+    const hashedPassword = hashPassword(password);
     const newUser = await prisma.user.create({
       data: {
         password: hashedPassword,
@@ -44,73 +45,97 @@ app.post('/users/create', async (req, res) => {
     });
 
     return res.status(200).json({
-      message: 'user created',
-      user: _.omit(newUser, 'password')
-    })
+      message: "user created",
+      user: _.omit(newUser, "password"),
+    });
   } catch (err) {
-    console.log('[error]', err)
-    res.status(500).json({ message: err.message })
+    console.log("[error]", err);
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
-app.post('/users/login', async (req, res) => {
+app.post("/users/login", async (req, res) => {
   try {
-    const { email, password } = req.body
+    const { email, password } = req.body;
     const user = await prisma.user.findUniqueOrThrow({
       where: {
-        email: email
-      }
-    })
+        email: email,
+      },
+    });
 
     if (!verifyPassword(password, process.env.SALT, user.password)) {
-      return res.status(403).json({ message: 'Unauthorized' })
+      return res.status(403).json({ message: "Unauthorized" });
     }
-    const token = jwt.sign(user, process.env.JWT_KEY)
+    const token = jwt.sign(_.omit(user, "password"), process.env.JWT_KEY);
 
-    return res.status(200).json({ token })
+    return res.status(200).json({ token });
   } catch (err) {
-    console.log('[error]', err)
-    res.status(500).json({ message: err.message })
+    console.log("[error]", err);
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
-app.get('/', async (req, res) => {
-  return res.status(200).json({ "route": "index" });
-})
+app.get("/", async (req, res) => {
+  return res.status(200).json({ route: "index" });
+});
 
-app.get('/users', isAuthorized, async (req, res) => {
+app.get("/users", isAuthorized, async (req, res) => {
   try {
     const users = await prisma.user.findMany({
       select: {
         email: true,
-        username: true
-      }
-    })
+        username: true,
+      },
+    });
 
     return res.status(200).json(users);
   } catch (err) {
-    console.log('[error]', err)
-    res.status(500).json({ message: err.message })
+    console.log("[error]", err);
+    res.status(500).json({ message: err.message });
   }
-})
+});
+
+app.post("/order/create", isAuthorized, async (req, res) => {
+  try {
+    const { order, price } = req.body;
+    const { id } = req.user;
+    const newOrder = await prisma.order.create({
+      data: {
+        text: order,
+        price,
+        userId: id,
+      },
+    });
+
+    return res.status(200).json({
+      message: "order created",
+      order: _.pick(newOrder, ["text", "price"]),
+    });
+  } catch (err) {
+    console.log("[error]", err);
+    res.status(500).json({ message: err.message });
+  }
+});
 
 const server = app.listen(process.env.PORT, () => {
-  console.log(`App listening on port ${process.env.PORT}`)
-})
+  console.log(`App listening on port ${process.env.PORT}`);
+});
 
 const shutDown = async () => {
   await prisma.$disconnect();
 
   server.close(() => {
-    console.log('Closed out remaining connections');
+    console.log("Closed out remaining connections");
     process.exit(0);
   });
 
   setTimeout(() => {
-    console.error('Could not close connections in time, forcefully shutting down');
+    console.error(
+      "Could not close connections in time, forcefully shutting down"
+    );
     process.exit(1);
   }, 10000);
-}
+};
 
-process.on('SIGTERM', shutDown);
-process.on('SIGINT', shutDown);
+process.on("SIGTERM", shutDown);
+process.on("SIGINT", shutDown);
